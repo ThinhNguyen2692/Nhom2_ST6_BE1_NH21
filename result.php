e8e79f8de34e4818cc18e8c024718193f56c1034
huy30112002 <huy30112002@gmail.com>
2021-11-20 21:41:05 +0700

    demo20/11

--- result.php
+++ result.php
@@ -238,11 +238,22 @@
 						<!-- store products -->
 						<div class="row">
 							<?php
-							if(isset($_GET['keyword'])){
+							if(isset($_GET['keyword'])):
+								if (isset($_GET['searchCol'])):
 								$keyword = $_GET['keyword'];
+								$searchCol = $_GET['searchCol'];
 								$search = $product->search($keyword);
-								foreach($search as $value): ?>
-									<div class="col-md-4 col-xs-6">
+
+								$perPage = 6;
+								$page = isset($_GET['page'])?$_GET['page']:1; 			
+								$total = count($search); 					
+								$url = $_SERVER['PHP_SELF']."?searchCol=".$searchCol. "&keyword".$keyword;
+								$get3ProductsByType = $product -> get3ProductsByType($search, $page, $perPage);
+								foreach($get3ProductsByType as $value): 
+								if($searchCol == $value['type_id'] || $searchCol == 0):
+								?>
+
+								<div class="col-md-4 col-xs-6">
 								<div class="product">
 									<div class="product-img">
 										<img src="./img/<?php echo $value['image']?>" alt="">
@@ -273,9 +284,9 @@
 									</div>
 								</div>
 							</div>
-									<?php endforeach;
-
-							}
+									<?php
+									endif;
+									 endforeach;
 									?>
 
 
@@ -293,13 +304,18 @@
 							<span class="store-qty">Showing 20-100 products</span>
 							<ul class="store-pagination">
 								<li class="active">1</li>
-								<li><a href="#">2</a></li>
+								<!-- <li><a href="#">2</a></li>
 								<li><a href="#">3</a></li>
 								<li><a href="#">4</a></li>
 								<li><a href="#"><i class="fa fa-angle-right"></i></a></li>
+								<?php echo $product->paginate($url, $total, $perPage,$page); ?> -->
 							</ul>
 						</div>
 						<!-- /store bottom filter -->
+						<?php 
+						endif;
+						endif;
+						?>
 					</div>
 					<!-- /STORE -->
 				</div>
@@ -309,4 +325,4 @@
 		</div>
 		<!-- /SECTION -->
 
-<?php include "footer.html"; ?>
+<?php include "footer.html" ?>
