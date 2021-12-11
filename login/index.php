3c0e9a539180e758b7eac218770aad67ed1f32a0
ThinhNguyen2692 <86168944+ThinhNguyen2692@users.noreply.github.com>
2021-12-11 12:15:19 +0700

    demo11_12

--- login/index.php
+++ login/index.php
@@ -31,24 +31,24 @@
 				</div>
 			</div>
 			<div class="card-body">
-				<form>
+				<form action="login.php" method="post">
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
 						</div>
-						<input type="text" class="form-control" placeholder="username">
+						<input type="text" class="form-control" placeholder="numberphone" name="sdt">
 					</div>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-key"></i></span>
 						</div>
-						<input type="password" class="form-control" placeholder="password">
+						<input type="password" class="form-control" placeholder="password" name="password">
 					</div>
 					<div class="row align-items-center remember">
 						<input type="checkbox">Remember Me
 					</div>
 					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
+						<input name="submit" type="submit" value="Login" class="btn float-right login_btn">
 					</div>
 				</form>
 			</div>
