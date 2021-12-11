94f0807a67807adc0281c91b6ed7cced9a7c14b9
ThinhNguyen2692 <86168944+ThinhNguyen2692@users.noreply.github.com>
2021-12-11 12:16:59 +0700

    demo11_12

--- models/user.php
+++ models/user.php
@@ -1,18 +1,25 @@
 <?php
-    class Db
-    {
-       public function checkLogin($username, $password) {
-        $sql = self::$connection->prepare("SELECT * FROM users 
-        Where `username` =? and `password` =?");
-        $password = md5($password);
-        $sql->bind_param("ss",$username,$password);
-        $sql->execute();
-        $items = array();
-        $items = $sql->get_result()->num_rows;
-        if($items == 1) {
-            return true;
-        }else{
-            return false;
+    class User extends Db {
+        public function checkLogin($sdt,$pass){
+            $sql = self::$connection->prepare("SELECT * FROM user where `sodienthoai`=? AND `password`=?");
+            $sql->bind_param("ss",$sdt, $pass);
+            $sql->execute(); //return an object
+            $items = array();
+            $items = $sql->get_result()->num_rows;
+            if($items == 1){
+                return true;
+            }
+            else {
+                return false;
+            }
         }
-       }
+        public function checkid($sdt){
+            $sql = self::$connection->prepare("SELECT * FROM user where `sodienthoai`=? ");
+            $sql->bind_param("s",$sdt);
+            $sql->execute(); //return an object
+            $items = array();
+            $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+            return $items;
+        }
     }
+?>
