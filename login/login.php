b0930550a4ad09f1fc5edec66b74508764033be7
ThinhNguyen2692 <86168944+ThinhNguyen2692@users.noreply.github.com>
2021-12-11 12:18:20 +0700

    demo11_12

---
+++ login/login.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+require "../config.php";
+require "../models/db.php";
+require "../models/user.php";
+$user = new User;
+if(isset($_POST['submit'])){
+    $sdt = $_POST['sdt'];
+    $passwor = md5($_POST['password']);
+    if($user->checkLogin($sdt,$passwor)){
+      $getUser =  $user->checkid($sdt);
+      foreach ($getUser as $value) {
+        $_SESSION['id'] = $value['role_id'];
+      }
+      if($_SESSION['id'] == 0){
+        header('location:../admin/index.php');
+      }else  header('location:../index.php');
+    }else{
+        header('location:index.php');
+    }
+}
