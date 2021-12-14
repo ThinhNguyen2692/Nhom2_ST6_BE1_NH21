c401c3313fd1bfb71e88cb36b9ae6da48ac06992
ThinhNguyen2692 <nndthinh2802@gmail.com>
2021-12-14 13:13:08 +0700

    demo14_12/6

--- admin/models/db.php
+++ admin/models/db.php
@@ -15,7 +15,7 @@
             if (!self::$connection) {
             self::$connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME,PORT);
             self::$connection->set_charset(DB_CHARSET);
-            }
+            }   
             return self::$connection;
         }
     }
