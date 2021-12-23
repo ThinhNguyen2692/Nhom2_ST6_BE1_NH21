fcceab6c51fbc9670587e311a88d4cd67e01377c
ThinhNguyen2692 <nndthinh2802@gmail.com>
2021-12-23 20:34:36 +0700

    demo23_12/4

--- index.php
+++ index.php
@@ -274,7 +274,7 @@ include "header.php";
 		<div class="row">
 			<div class="col-md-4 col-xs-6">
 				<div class="section-title">
-					<h4 class="title">Top selling</h4>
+					<h4 class="title">Top selling 1</h4>
 					<div class="section-nav">
 						<div id="slick-nav-3" class="products-slick-nav"></div>
 					</div>
@@ -307,7 +307,7 @@ include "header.php";
 
 			<div class="col-md-4 col-xs-6">
 				<div class="section-title">
-					<h4 class="title">Top selling2</h4>
+					<h4 class="title">Top selling 2</h4>
 					<div class="section-nav">
 						<div id="slick-nav-4" class="products-slick-nav"></div>
 					</div>
@@ -347,7 +347,7 @@ include "header.php";
 
 			<div class="col-md-4 col-xs-6">
 				<div class="section-title">
-					<h4 class="title">Top selling</h4>
+					<h4 class="title">Top selling 3</h4>
 					<div class="section-nav">
 						<div id="slick-nav-5" class="products-slick-nav"></div>
 					</div>
